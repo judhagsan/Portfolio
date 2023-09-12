00fea26506d4f9d3bf358feea7987c712bdd00c4
judhagsan <133703433+judhagsan@users.noreply.github.com>
2023-09-12 16:10:23 +0000

    testes adicionados

---
+++ models/calculadora.js
@@ -0,0 +1,19 @@
+function somar(argumento1, argumento2) {
+  return argumento1 + argumento2;
+}
+
+function subtrair(argumento1, argumento2) {
+  return argumento1 - argumento2;
+}
+
+function multiplicar(argumento1, argumento2) {
+  return argumento1 * argumento2;
+}
+
+function dividir(argumento1, argumento2) {
+  return argumento1 / argumento2;
+}
+exports.somar = somar;
+exports.subtrair = subtrair;
+exports.multiplicar = multiplicar;
+exports.dividir = dividir;
--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,5 +1,21 @@
-test("nome do teste", callBackFunction);
+const calculadora = require("../models/calculadora.js");
 
-function callBackFunction() {
-  console.log("essa funçao ?");
-}
+test("somar 2 + 2 devia retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("subtrair 2 - 2 devia retornar 0", () => {
+  const resultado = calculadora.subtrair(2, 2);
+  expect(resultado).toBe(0);
+});
+
+test("multiplicar 2 * 2 devia retornar 4", () => {
+  const resultado = calculadora.multiplicar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("dividir 2 / 2 devia retornar 1", () => {
+  const resultado = calculadora.dividir(2, 2);
+  expect(resultado).toBe(1);
+});
