906ea5443df43fe2e3bae5981c3b45d8c198a6ba
judhagsan <133703433+judhagsan@users.noreply.github.com>
2024-02-25 19:32:59 +0000

    Adicionando os arquivos para testar imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import perfil from "pages/imgs/Pixel_eu.png";
+
 function Home() {
-  return <h1>| Judhagsan em construção |</h1>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col text-center">
+          <img
+            src={perfil}
+            className="rounded mx-auto d-block"
+            alt="Descrição da Imagem"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
