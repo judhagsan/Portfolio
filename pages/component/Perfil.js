d83e9865e9e72e13466732741642d5718b67b72a
judhagsan <133703433+judhagsan@users.noreply.github.com>
2024-03-13 23:50:45 +0000

    card 1 feito

--- pages/component/Perfil.js
+++ pages/component/Perfil.js
@@ -4,15 +4,8 @@ import avatar from "public/pixel-perfil.png"; // Importação correta
 
 function Perfil() {
   return (
-    // Utiliza classes do Tailwind para borda arredondada, largura, altura e overflow
-    <div className="rounded-full overflow-hidden">
-      <Image
-        className=" h-20 w-20 sm:h-45 sm:w-45 md:h-45 md:w-45 lg:h-45 lg:w-45 xl:h-45 xl:w-45 2xl:h-45 2xl:w-45"
-        src={avatar}
-        alt="Perfil"
-        objectFit="cover"
-        quality={100}
-      />
+    <div className="rounded-full overflow-hidden w-40 md:w-52 z-20">
+      <Image src={avatar} alt="Perfil" quality={100} />
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import React from "react";
-import Avatar from "../pages/component/Perfil"; // Importação correta
+import React, { useState } from "react";
+import Avatar from "../pages/component/Perfil";
+import Card from "../pages/component/Card";
+import image from "../public/pixel-perfil.png";
+import image2 from "../public/OFR-55.jpg";
 
 function Home() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <Avatar />
+    <div className="relative w-full h-full">
+      <div className="flex justify-center items-center h-screen w-screen">
+        <Avatar />
+      </div>
+      <div className="absolute inset-1/2">
+        <Card image={image} />
+      </div>
     </div>
   );
 }
